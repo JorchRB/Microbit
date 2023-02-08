a7f61d881d39b08a6c58d0ab87d0ea1a1cc56347
JorchRB <114919867+JorchRB@users.noreply.github.com>
2023-02-08 13:43:15 +0100

    Create tarea6b.py

---
+++ tarea6b.py
@@ -0,0 +1,8 @@
+# Imports go at the top
+from microbit import *
+
+import music
+
+Himno_de_España = ["f4:4", "c4:4", "a4:4", "f4:2", "c5:2", "bb4:2", "a4:2", "g4:2", "f4:2", "f4:2", "e4:2", "d4:2", "c4:2", "f4:4", "g4:4", "a4:4", "", "c4:2", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]
+
+music.play(Himno_de_España)
